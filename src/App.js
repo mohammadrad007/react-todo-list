76559f6a5c768969dfd1a94c05a6c8aa610668e2
Mohammad Mohammadi <saberzx313@gmail.com>
2019-06-08 12:59:54 +0430

    add Fontawsome and UI component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import uuid from "uuid";
 class App extends Component {
+  state = {
+    items: [],
+    id: 0,
+    item: "",
+    editItem: false
+  };
+  handleChange = e => {
+    this.setState({
+      item: e.target.value
+    });
+  };
   render() {
-    return <div>Hello From App cmp</div>;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">Todo input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+            />
+            <TodoList />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 export default App;
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { directive } from "@babel/types";
 
 class TodoInput extends Component {
   render() {
-    return <div>Hello From TODO INPUt</div>;
+    return (
+      <directive className="card card-body my-3">
+        <form>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book" />
+              </div>
+            </div>
+            <input
+              type="text"
+              className="form-control text-capitalize"
+              placeholder="add a todo item"
+            />
+          </div>
+          <button type="submit" className="btn btn-block btn-primary mt-3">
+            add item
+          </button>
+        </form>
+      </directive>
+    );
   }
 }
 export default TodoInput;
