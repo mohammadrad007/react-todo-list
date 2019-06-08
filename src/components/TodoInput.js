b01bc1d79f0f40e0a11e54c3c628883f3f6f67bb
Mohammad Mohammadi <saberzx313@gmail.com>
2019-06-08 12:15:39 +0430

    TodoList and TodoIte component>>

---
+++ src/components/TodoInput.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class TodoInput extends Component {
+  render() {
+    return <div>Hello From TODO INPUt</div>;
+  }
+}
+export default TodoInput;
---
+++ src/components/TodoList.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class TodoList extends Component {
+  render() {
+    return <div>Hello From Todo List</div>;
+  }
+}
+export default TodoList;
